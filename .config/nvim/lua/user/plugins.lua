07083e0b04abb445f37173735bd1468ab155b422
Jacob Koziej <jacobkoziej@gmail.com>
2022-01-12 01:36:07 -0500

    Install `nvim-treesitter`

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -3,6 +3,10 @@ local packer = require "packer"
 return packer.startup(
 function(use)
 	use "arcticicestudio/nord-vim"
+	use {
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	}
 	use "wbthomason/packer.nvim"
 end
 )
