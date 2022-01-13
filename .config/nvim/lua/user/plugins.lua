a126a8124011ab3c69bf7264803a333de9147328
Jacob Koziej <jacobkoziej@gmail.com>
2022-01-13 00:07:00 -0500

    Install `editorconfig-vim`

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -3,6 +3,7 @@ local packer = require "packer"
 return packer.startup(
 function(use)
 	use "arcticicestudio/nord-vim"
+	use "editorconfig/editorconfig-vim"
 	use {
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
