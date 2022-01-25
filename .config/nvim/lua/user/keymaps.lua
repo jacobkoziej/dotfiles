a6e373ad2740ae8e8180c7ad05b033ace31fbf9e
Jacob Koziej <jacobkoziej@gmail.com>
2022-01-24 22:29:29 -0500

    Remap leader key to `<Space>`

---
+++ .config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,10 @@
+local keymap = vim.api.nvim_set_keymap
+
+local opts = {
+	noremap = true,
+	silent  = true,
+}
+
+-- remap <leader> to space
+keymap("n", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
