a61554c39acfa000dd633259b109f3b1f861b9c4
Jacob Koziej <jacobkoziej@gmail.com>
2022-01-26 17:04:27 -0500

    Install `lewis6991/gitsigns.nvim`

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -6,6 +6,7 @@ function(use)
 	use "editorconfig/editorconfig-vim"
 	use "kyazdani42/nvim-tree.lua"
 	use "kyazdani42/nvim-web-devicons"
+	use "lewis6991/gitsigns.nvim"
 	use "nvim-lua/plenary.nvim"
 	use {
 		"nvim-treesitter/nvim-treesitter",
