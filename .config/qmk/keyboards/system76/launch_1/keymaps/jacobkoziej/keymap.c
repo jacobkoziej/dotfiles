e58dd3adad1f846eab5aa3a4d96222088f94b654
Jacob Koziej <jacobkoziej@gmail.com>
2022-03-07 12:24:18 -0500

    Restructure default keyboard layout

--- .config/qmk/keyboards/system76/launch_1/keymaps/jacobkoziej/keymap.c
+++ .config/qmk/keyboards/system76/launch_1/keymaps/jacobkoziej/keymap.c
@@ -20,13 +20,13 @@
  * Layout
  *  _________________________________________________________________________________________________________________________________  ________
  * |        |        |        |        |        |        |        |        |        |        |        |        |        |            ||        |
- * |  ESC   |   F1   |   F2   |   F3   |   F4   |   F5   |   F6   |   F7   |   F8   |   F9   |  F10   |  F11   |  F12   |   DELETE   ||  HOME  |
+ * |  ESC   |   F1   |   F2   |   F3   |   F4   |   F5   |   F6   |   F7   |   F8   |   F9   |  F10   |  F11   |  F12   |   DELETE   ||  PGUP  |
  * |________|________|________|________|________|________|________|________|________|________|________|________|________|____________||________|
  * |   ~    |   !    |   @    |   #    |   $    |   %    |   ^    |   &    |   *    |   (    |   )    |   _    |   +    |            ||        |
- * |   `    |   1    |   2    |   3    |   4    |   5    |   6    |   7    |   8    |   9    |   0    |   -    |   =    | BACKSPACE  ||  PGUP  |
+ * |   `    |   1    |   2    |   3    |   4    |   5    |   6    |   7    |   8    |   9    |   0    |   -    |   =    | BACKSPACE  ||  PGDN  |
  * |________|________|________|________|________|________|________|________|________|________|________|________|________|____________||________|
  * |            |        |        |        |        |        |        |        |        |        |        |   [    |   ]    |   |    ||        |
- * |    TAB     |   Q    |   W    |   E    |   R    |   T    |   Y    |   U    |   I    |   O    |   P    |   {    |   }    |   \    ||  PGDN  |
+ * |    TAB     |   Q    |   W    |   E    |   R    |   T    |   Y    |   U    |   I    |   O    |   P    |   {    |   }    |   \    ||  HOME  |
  * |____________|________|________|________|________|________|________|________|________|________|________|________|________|________||________|
  *   |            |        |        |        |        |        |        |        |        |        |   :    |   "    |            |   |        |
  *   |    CAPS    |   A    |   S    |   D    |   F    |   G    |   H    |   J    |   K    |   L    |   ;    |   '    |    ENTER   |   |  END   |
@@ -35,7 +35,7 @@
  *   |     SHIFT      |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |   ,    |   .    |   /    |   SHIFT    |   UP   |
  *   |________________|________|________|________|________|________|________|________|________|________|________|____________|________|________
  *   |            |        |       |        |                 |                 |        |        |             |   |        |        |        |
- *   |    CTRL    |  LALT  |  FN1  |  LGUI  |      SPACE      |      SPACE      | RCTRL  |  RALT  |     FN1     |   |  LEFT  |  DOWN  | RIGHT  |
+ *   |    CTRL    |  LALT  | LGUI  |  FN2   |      SPACE      |       FN1       |  RALT  | RCTRL  |     FN3     |   |  LEFT  |  DOWN  | RIGHT  |
  *   |____________|________|_______|________|_________________|_________________|________|________|_____________|   |________|________|________|
  */
 
@@ -44,12 +44,12 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-	KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
-	KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_PGUP,
-	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN,
+	KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_PGUP,
+	KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_PGDN,
+	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
 	KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,           KC_END,
 	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,
-	KC_LCTL, KC_LALT, MO(1),   KC_LGUI,          KC_SPC,  KC_SPC,           KC_RCTL, KC_RALT, MO(1),            KC_LEFT, KC_DOWN, KC_RGHT
+	KC_LCTL, KC_LALT, KC_LGUI, MO(2),            KC_SPC,  MO(1),            KC_RALT, KC_RCTL, MO(3),            KC_LEFT, KC_DOWN, KC_RGHT
 	),
 
 	[1] = LAYOUT(
