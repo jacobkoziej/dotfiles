0c985f3eaa5edb6c4990195cf4945a9ddb6003a2
Jacob Koziej <jacobkoziej@gmail.com>
2022-06-11 00:58:32 -0400

    Add support to bootstrap nvim

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,3 +1,17 @@
+local packer_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local packer_bootstrap = nil
+
+if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
+	print 'bootstrapping nvim...'
+	packer_bootstrap = vim.fn.system {
+		'git',
+		'clone',
+		'https://github.com/wbthomason/packer.nvim',
+		packer_path,
+	}
+	vim.cmd 'packadd packer.nvim'
+end
+
 local packer = require 'packer'
 
 return packer.startup(
@@ -10,8 +24,17 @@ function(use)
 	use 'nvim-lua/plenary.nvim'
 	use {
 		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate',
+		run = vim.cmd [[
+			try
+				TSUpdate
+			catch /.*/
+			endtry
+		]],
 	}
 	use 'wbthomason/packer.nvim'
+
+	if packer_bootstrap then
+		packer.sync()
+	end
 end
 )
