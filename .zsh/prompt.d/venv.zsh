a6c2cc9a916e6a6a34b8da152be26a66ff587eb3
Jacob Koziej <jacobkoziej@gmail.com>
2022-07-17 01:02:03 -0400

    Add venv info to `PS1`

---
+++ .zsh/prompt.d/venv.zsh
@@ -0,0 +1,22 @@
+VENV_GENERIC_NAMES=(
+	'.venv'
+	'venv'
+	'virtualenv'
+)
+
+prompt::section::venv()
+{
+	[[ -n "$VIRTUAL_ENV" ]] || return
+
+	local name
+
+	if [[ "${VENV_GENERIC_NAMES[(i)$VIRTUAL_ENV:t]}" -le "${#VENV_GENERIC_NAMES}" ]]; then
+		name="$VIRTUAL_ENV:h:t"
+	elif [[ "$VIRTUAL_ENV" != "${VIRTUAL_ENV#$PYTHONVENV/}" ]]; then
+		name="${VIRTUAL_ENV#$PYTHONVENV/}"
+	else
+		name="$VIRTUAL_ENV:t"
+	fi
+
+	echo -n " %F{yellow}$name%f"
+}
