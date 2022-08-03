79463a4e663bec0968074c080cbaa2c68b0b004b
Jacob Koziej <jacobkoziej@gmail.com>
2022-08-03 16:45:22 -0400

    Strip non-git components of pwd

--- .zsh/prompt.d/dir.zsh
+++ .zsh/prompt.d/dir.zsh
@@ -1,12 +1,27 @@
 prompt::section::dir()
 {
+	local GIT_GENERIC_NAME='.git'
 	local RO_SYMBOL='ro'
 
-	local dir
+	local dir git_dir git_prefix
 
-	dir="${dir}%F{cyan}%~%f"
+	if git_prefix=$(git rev-parse --show-toplevel 2> /dev/null); then
+		# determine true path to git directory
+		git_dir=$(git rev-parse \
+			--path-format=absolute \
+			--git-common-dir \
+			2> /dev/null)
 
-	[[ ! -w . ]] && dir="${dir} %F{red}$RO_SYMBOL%f"
+		# determine repo name
+		[[ "$git_dir:t" = "$GIT_GENERIC_NAME" ]] &&
+			dir="$git_dir:h:t" || dir="$git_dir:t"
 
-	echo -n "$dir"
+		# remove non-git path components
+		dir="$dir${${PWD:A}#$~~git_prefix}"
+	else
+		dir="%~"
+	fi
+
+	echo -n "%F{cyan}${dir}%f"
+	[[ ! -w . ]] && echo -n " %F{red}$RO_SYMBOL%f"
 }
