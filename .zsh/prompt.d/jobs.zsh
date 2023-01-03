cc09b109a1d0243e8c6d5ce9c5bb3b08bbfb8e34
Jacob Koziej <jacobkoziej@gmail.com>
2023-01-02 22:49:00 -0500

    Add jobs info to `PS1`

---
+++ .zsh/prompt.d/jobs.zsh
@@ -0,0 +1,4 @@
+prompt::section::jobs()
+{
+	[[ ${#jobstates} -gt 0 ]] && echo -n '%F{magenta}*%f'
+}
--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,17 +1,20 @@
 source "$HOME/.zsh/prompt.d/dir.zsh"
 source "$HOME/.zsh/prompt.d/git.zsh"
+source "$HOME/.zsh/prompt.d/jobs.zsh"
 source "$HOME/.zsh/prompt.d/venv.zsh"
 
 prompt::ps1()
 {
 	local section_git="$(prompt::section::git)"
+	local section_jobs="$(prompt::section::jobs)"
 	local section_venv="$(prompt::section::venv)"
 
 	[[ -n "$section_git" ]] && section_git=" $section_git"
+	[[ -n "$section_jobs" ]] && section_jobs=" $section_jobs"
 	[[ -n "$section_venv" ]] && section_venv=" $section_venv"
 
 	echo "%B$(prompt::section::dir)$section_git$section_venv"
-	echo -n "[ZSH] %F{magenta}%(!.#.>)%f%b "
+	echo -n "[ZSH]$section_jobs %F{magenta}%(!.#.>)%f%b "
 }
 
 prompt::ps2()
