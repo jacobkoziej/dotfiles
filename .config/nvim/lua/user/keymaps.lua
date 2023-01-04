234ba460af8f9afcc11eb6bab1bb80fca362e4a2
Jacob Koziej <jacobkoziej@gmail.com>
2023-01-04 01:26:18 -0500

    Move line(s) in visual mode with auto indentation

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -13,6 +13,10 @@ vim.keymap.set('n', 'J',     'mzJ`z',   opts)
 vim.keymap.set('n', 'N',     'Nzz',     opts)
 vim.keymap.set('n', 'n',     'nzz',     opts)
 
+-- move line(s) in visual mode with auto indentation
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", opts)
+
 -- files
 vim.keymap.set('n', '<leader>fe', ':NvimTreeToggle<CR>', opts)
 vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', opts)
