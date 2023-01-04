8ec57887dd70bc61b3460c171b6dba991554387d
Jacob Koziej <jacobkoziej@gmail.com>
2023-01-04 00:59:26 -0500

    Utilize `keymap` module

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -1,14 +1,11 @@
-local keymap = vim.api.nvim_set_keymap
-
 local opts = {
-	noremap = true,
 	silent  = true,
 }
 
 -- remap <leader> to space
-keymap('n', '<Space>', '<Nop>', opts)
+vim.keymap.set('n', '<Space>', '<Nop>', opts)
 vim.g.mapleader = ' '
 
 -- files
-keymap('n', '<leader>fe', ':NvimTreeToggle<CR>', opts)
-keymap('n', '<leader>ff', ':Telescope find_files<CR>', opts)
+vim.keymap.set('n', '<leader>fe', ':NvimTreeToggle<CR>', opts)
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', opts)
