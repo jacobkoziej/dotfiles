ccdd24725dd7dac5ded9f034142ed90713133086
Jacob Koziej <jacobkoziej@gmail.com>
2023-01-04 01:10:56 -0500

    Remap movement keys to make them feel more consistent

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -6,6 +6,13 @@ local opts = {
 vim.keymap.set('n', '<Space>', '<Nop>', opts)
 vim.g.mapleader = ' '
 
+-- more consistent feeling movements
+vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
+vim.keymap.set('n', 'J',     'mzJ`z',   opts)
+vim.keymap.set('n', 'N',     'Nzz',     opts)
+vim.keymap.set('n', 'n',     'nzz',     opts)
+
 -- files
 vim.keymap.set('n', '<leader>fe', ':NvimTreeToggle<CR>', opts)
 vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', opts)
