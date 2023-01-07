2ca8e3308fc25f442270b8ed3938e15738a15416
Jacob Koziej <jacobkoziej@gmail.com>
2023-01-06 19:54:31 -0500

    Return programs from module

--- .config/awesome/jacobkoziej/programs.lua
+++ .config/awesome/jacobkoziej/programs.lua
@@ -1,7 +1,8 @@
 local programs = { }
 
-local browswer = os.getenv('BROWSER')  or 'librewolf'
-local editor   = os.getenv('EDITOR')   or 'nvim'
-local terminal = os.getenv('TERMINAL') or 'alacritty'
+-- generic
+programs.browswer = os.getenv('BROWSER')  or 'librewolf'
+programs.editor   = os.getenv('EDITOR')   or 'nvim'
+programs.terminal = os.getenv('TERMINAL') or 'alacritty'
 
 return programs
