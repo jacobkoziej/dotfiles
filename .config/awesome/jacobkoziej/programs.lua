243930ddbf200fc44f37e5e2580323eaf23ac46c
Jacob Koziej <jacobkoziej@gmail.com>
2023-01-07 15:31:10 -0500

    Add specific programs

--- .config/awesome/jacobkoziej/programs.lua
+++ .config/awesome/jacobkoziej/programs.lua
@@ -5,4 +5,16 @@ programs.browswer = os.getenv('BROWSER')  or 'librewolf'
 programs.editor   = os.getenv('EDITOR')   or 'nvim'
 programs.terminal = os.getenv('TERMINAL') or 'alacritty'
 
+-- specific
+programs.arandr     = 'arandr'
+programs.discord    = 'discord'
+programs.dmenu      = 'dmenu_run'
+programs.lockscreen = 'slock'
+programs.pactl      = 'pactl'
+programs.pass       = 'passmenu'
+programs.playerctl  = 'playerctl'
+programs.screenkey  = 'screenkey'
+programs.signal     = 'signal'
+programs.xbacklight = 'xbacklight'
+
 return programs
