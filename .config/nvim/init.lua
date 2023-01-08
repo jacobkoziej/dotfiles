2cfe9768d293643ffad7262736f9bcfc9e195e9b
Jacob Koziej <jacobkoziej@gmail.com>
2023-01-08 01:04:43 -0500

    Make `init.lua` a one-liner

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,1 @@
-require 'jacobkoziej.keymap'
-require 'jacobkoziej.options'
-require 'jacobkoziej.plugins'
-require 'jacobkoziej.style'
+require 'jacobkoziej'
---
+++ .config/nvim/lua/jacobkoziej/init.lua
@@ -0,0 +1,4 @@
+require 'jacobkoziej.plugins'
+require 'jacobkoziej.options'
+require 'jacobkoziej.keymap'
+require 'jacobkoziej.style'
