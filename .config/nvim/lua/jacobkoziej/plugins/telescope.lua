ab4deb6eb2ee803cd2d713f5a18c5efb9417b838
Jacob Koziej <jacobkoziej@gmail.com>
2023-06-25 22:31:11 -0700

    Set default `telescope` theme to `ivy`

--- .config/nvim/lua/jacobkoziej/plugins/telescope.lua
+++ .config/nvim/lua/jacobkoziej/plugins/telescope.lua
@@ -1,17 +1,19 @@
 local ok, telescope = pcall(require, 'telescope')
 if not ok then return end
 
+local ok, themes = pcall(require, 'telescope.themes')
+if not ok then return end
+
 telescope.setup {
-	defaults = {
+	defaults = vim.tbl_extend('force', themes.get_ivy(), {
 		file_ignore_patterns = {
 			'.git/',
 		},
-	},
+	}),
 
 	pickers = {
 		find_files = {
 			hidden = true,
-			theme  = 'ivy',
 		},
 	},
 }
