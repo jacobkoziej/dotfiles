3c72aade031f39c6c7e9f6b40de24a0dd256ca0f
Jacob Koziej <jacobkoziej@gmail.com>
2024-02-25 15:52:42 -0500

    Add number row

--- .config/qmk/keyboards/moonlander/keymaps/jacobkoziej/keymap.c
+++ .config/qmk/keyboards/moonlander/keymaps/jacobkoziej/keymap.c
@@ -21,11 +21,11 @@ enum layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT(
-	QK_BOOT, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
+	KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , _______,          _______, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
 	_______, KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , _______,          _______, KC_J   , KC_L   , KC_U   , KC_Y   , KC_QUOT, _______,
 	_______, KC_A   , MT_R   , MT_S   , MT_T   , KC_G   , _______,          _______, KC_M   , MT_N   , MT_E   , MT_I   , KC_O   , _______,
 	_______, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   ,                            KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, _______,
-	_______, _______, _______, _______, KC_ESC ,          _______,          _______,          KC_LSFT, _______, _______, _______, _______,
+	QK_BOOT, _______, _______, _______, KC_ESC ,          _______,          _______,          KC_RSFT, _______, _______, _______, _______,
 	                                    KC_SPC , KC_TAB , _______,          _______, KC_ENT , KC_BSPC
 	),
 };
