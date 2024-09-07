d5aae993ac5930cfd91d4760d7254f4d69baba83
Jacob Koziej <jacobkoziej@gmail.com>
2024-09-07 18:37:33 -0400

    Set `TAPPING_TERM` to 150 ms

--- .config/qmk/keyboards/zsa/moonlander/keymaps/jacobkoziej/config.h
+++ .config/qmk/keyboards/zsa/moonlander/keymaps/jacobkoziej/config.h
@@ -4,4 +4,8 @@
  * Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
  */
 
+#pragma once
+
+#include <shared/config.h>
+
 #define LAYOUT_COLEMAK_DHM
--- .config/qmk/shared/config.h
+++ .config/qmk/shared/config.h
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 /*
- * config.h -- launch_1 config
+ * config.h -- shared config
  * Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
  */
 
 #pragma once
 
-#include <shared/config.h>
-
-#define LAYOUT_QWERTY
+#define TAPPING_TERM 150
