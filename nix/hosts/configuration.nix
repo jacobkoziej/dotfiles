76d306a2a56f77e01500a9b0345695bd95b1666a
Jacob Koziej <jacobkoziej@gmail.com>
2025-04-23 21:37:50 -0400

    Add `wheel` group to `trusted-users`

--- nix/hosts/configuration.nix
+++ nix/hosts/configuration.nix
@@ -19,6 +19,9 @@
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    trusted-users = [
+      "@wheel"
+    ];
   };
 
   nixpkgs.config.allowUnfree = true;
