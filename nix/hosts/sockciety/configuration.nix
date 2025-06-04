acbaac2279493d22cd63f8710fc519c88c63fece
Jacob Koziej <jacobkoziej@gmail.com>
2025-06-04 07:57:58 -0400

    feat: configure `services.snapraid`

--- nix/hosts/sockciety/configuration.nix
+++ nix/hosts/sockciety/configuration.nix
@@ -9,6 +9,32 @@
     ./hardware-configuration.nix
   ];
 
+  services = {
+    snapraid = {
+      enable = true;
+
+      extraConfig = ''
+        autosave 256
+      '';
+
+      contentFiles = [
+        "/var/lib/snapraid/big-chungus.content"
+        "/mnt/big-chungus.d/disk0/big-chungus.content"
+        "/mnt/big-chungus.d/disk1/big-chungus.content"
+      ];
+      dataDisks = {
+        "disk0" = "/mnt/big-chungus.d/disk0";
+        "disk1" = "/mnt/big-chungus.d/disk1";
+      };
+      exclude = [
+        "/lost+found/"
+      ];
+      parityFiles = [
+        "/mnt/big-chungus.d/parity0/big-chungus.parity"
+      ];
+    };
+  };
+
   networking.hostName = "sockciety";
 
   system.stateVersion = "25.05";
