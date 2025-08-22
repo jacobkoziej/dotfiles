3dcdf84a1c561fbfe3ca0708ac1cfc05c99c2c10
Jacob Koziej <jacobkoziej@gmail.com>
2025-08-21 23:58:02 -0400

    feat: allow remote unlock with `iceroth` ssh key

--- nix/modules/nixos/boot.nix
+++ nix/modules/nixos/boot.nix
@@ -18,6 +18,7 @@
           enable = true;
 
           authorizedKeys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINQeA0J16znzRrtqvh5xM014+CcIEetO8uu4dobmcMta jacobkoziej@iceroth.host.jacobkoziej.xyz"
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEYZmYvejeN8+3cw33v7U5dp4aOKbVqfhQ+aZRheMII jacobkoziej@voyager-1.host.jacobkoziej.xyz"
           ];
           hostKeys = [
