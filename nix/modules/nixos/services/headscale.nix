b2cd9b7b861c6c3721743b58b2b92cac9980b137
Jacob Koziej <jacobkoziej@gmail.com>
2025-08-24 21:19:52 -0400

    refactor: don't rely on `localhost` to resolve

--- nix/modules/nixos/services/headscale.nix
+++ nix/modules/nixos/services/headscale.nix
@@ -76,7 +76,7 @@ in
       enableACME = true;
 
       locations."/" = {
-        proxyPass = "http://localhost:${toString cfg.port}";
+        proxyPass = "http://127.0.0.1:${toString cfg.port}";
         proxyWebsockets = true;
       };
     };
