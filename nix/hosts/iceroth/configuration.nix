251ea97025857cf3521ffaabc7a6371b5ba97cf7
Jacob Koziej <jacobkoziej@gmail.com>
2025-09-02 12:33:25 -0400

    feat: set static IPv4 address

--- nix/hosts/iceroth/configuration.nix
+++ nix/hosts/iceroth/configuration.nix
@@ -1,15 +1,20 @@
 {
   config,
+  inputs,
   pkgs,
   ...
 }:
 
+let
+  inherit (inputs) secrets;
+
+  secrets-iceroth = secrets.hosts.iceroth;
+
+in
 {
   imports = [
     ./hardware-configuration.nix
   ];
-
-  networking.hostName = "iceroth";
 
   services = {
     udev.packages = with pkgs; [
@@ -17,6 +22,20 @@
     ];
     xserver.enable = true;
   };
+
+  systemd.network.networks = with secrets-iceroth.network; {
+    "10-en" = {
+      matchConfig = {
+        Name = "en0";
+      };
+      networkConfig = {
+        Address = address.v4 + "/" + subnet.v4;
+        Gateway = gateway.v4;
+      };
+    };
+  };
+
+  networking.hostName = "iceroth";
 
   system.stateVersion = "25.05";
 }
