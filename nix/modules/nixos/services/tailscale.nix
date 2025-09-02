e8d496db58ec2fc711439cbf97fb82a7476978ee
Jacob Koziej <jacobkoziej@gmail.com>
2025-09-02 00:20:05 -0400

    feat: add tailscale to `network-online.target`

--- nix/modules/nixos/services/tailscale.nix
+++ nix/modules/nixos/services/tailscale.nix
@@ -1,14 +1,47 @@
 {
+  config,
   lib,
   ...
 }:
 
 let
   inherit (lib) mkDefault;
+  inherit (lib) mkIf;
+
+  cfg = config.services.tailscale;
 
 in
 {
   services.tailscale = {
     enable = mkDefault true;
+  };
+
+  systemd.services.tailscale-online = mkIf cfg.enable {
+    wants = [
+      "tailscaled.service"
+    ];
+    after = [
+      "tailscaled.service"
+    ];
+
+    wantedBy = [
+      "network-online.target"
+    ];
+
+    serviceConfig =
+      let
+        inherit (builtins) toString;
+
+        cfg-systemd = config.systemd;
+
+        systemd-networkd-wait-online = "${cfg-systemd.package}/lib/systemd/systemd-networkd-wait-online";
+        timeout = toString cfg-systemd.network.wait-online.timeout;
+
+      in
+      {
+        ExecStart = "${systemd-networkd-wait-online} --interface=${cfg.interfaceName} -4 -6 --timeout=${timeout}";
+        Type = "oneshot";
+        RemainsAfterExit = true;
+      };
   };
 }
