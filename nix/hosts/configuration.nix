3b343cba8dff28895f97ccd280a772fbd3d842c5
Jacob Koziej <jacobkoziej@gmail.com>
2025-09-07 15:09:22 -0700

    feat: automatically optimise the Nix Store

--- nix/hosts/configuration.nix
+++ nix/hosts/configuration.nix
@@ -8,20 +8,26 @@
     ../modules/nixos
   ];
 
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    substituters = [
-      "https://nix-community.cachix.org/"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-    trusted-users = [
-      "@wheel"
-    ];
+  nix = {
+    optimise = {
+      automatic = true;
+      persistent = true;
+    };
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [
+        "https://nix-community.cachix.org/"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      trusted-users = [
+        "@wheel"
+      ];
+    };
   };
 
   nixpkgs = {
