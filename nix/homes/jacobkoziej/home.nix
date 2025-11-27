16bb5656014240f8271f9cbb2d2f43a36f903e98
Jacob Koziej <jacobkoziej@gmail.com>
2025-11-27 13:22:32 -0500

    fix: add `pkgs.iproute2` only on `linux`

--- nix/homes/jacobkoziej/home.nix
+++ nix/homes/jacobkoziej/home.nix
@@ -8,6 +8,8 @@ let
   inherit (pkgs) python3;
 
   inherit (lib) mkDefault;
+  inherit (lib) optionals;
+  inherit (pkgs.stdenv) isLinux;
 
   python3-pkgs = python3.withPackages (
     ps: with ps; [
@@ -22,37 +24,41 @@ in
   home = {
     username = mkDefault "jacobkoziej";
 
-    packages = with pkgs; [
-      bat
-      cachix
-      daemonize
-      duf
-      eza
-      fd
-      fzf
-      htop
-      inetutils
-      iproute2
-      killall
-      mosh
-      ncdu
-      neovim
-      openssh
-      pciutils
-      procps
-      python3-pkgs
-      riffdiff
-      ripgrep
-      rsync
-      sd
-      sshfs
-      tinyxxd
-      tmux
-      tmuxp
-      unzip
-      wget
-      zip
-      zoxide
-    ];
+    packages =
+      with pkgs;
+      [
+        bat
+        cachix
+        daemonize
+        duf
+        eza
+        fd
+        fzf
+        htop
+        inetutils
+        killall
+        mosh
+        ncdu
+        neovim
+        openssh
+        pciutils
+        procps
+        python3-pkgs
+        riffdiff
+        ripgrep
+        rsync
+        sd
+        sshfs
+        tinyxxd
+        tmux
+        tmuxp
+        unzip
+        wget
+        zip
+        zoxide
+      ]
+      ++ optionals isLinux [
+        iproute2
+      ];
   };
 }
