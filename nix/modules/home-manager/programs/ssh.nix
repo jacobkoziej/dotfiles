40aaa27985145cad34efd4892467d73dd36651fe
Jacob Koziej <jacobkoziej@gmail.com>
2025-11-27 01:41:49 -0500

    fix: copy `authorized_keys` from `/nix/store`
    Without copying `authorized_keys` from `/nix/store`, `sshd` will
    rightfully get upset about incorrect ownership permissions.

--- nix/modules/home-manager/programs/ssh.nix
+++ nix/modules/home-manager/programs/ssh.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 
@@ -29,5 +30,23 @@ in
   home.file.".ssh/authorized_keys" = {
     inherit (cfg) enable;
     text = mkDefault authorizedKeys;
+
+    # we copy authorized_keys from /nix/store to prevent sshd from
+    # rightfully getting upset about incorrect ownership permissions
+    force = true;
+    onChange =
+      let
+        inherit (lib) getExe';
+        inherit (pkgs) coreutils;
+
+        cp = getExe' coreutils "cp";
+        realpath = getExe' coreutils "realpath";
+
+        inherit (config.home.file.".ssh/authorized_keys") target;
+
+      in
+      ''
+        ${cp} --remove-destination $(${realpath} ${target}) ${target}
+      '';
   };
 }
