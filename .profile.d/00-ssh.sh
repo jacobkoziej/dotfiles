490a38b30d8dc08f104b9ed04898043ad3692e6b
Jacob Koziej <jacobkoziej@gmail.com>
2026-04-28 10:07:21 -0700

    fix: only symlink `SSH_AUTH_SOCK` if paths differ
    Without the following we can create a symlink to `$HOME/.ssh/auth_sock`.

--- .profile.d/00-ssh.sh
+++ .profile.d/00-ssh.sh
@@ -1,5 +1,7 @@
 if [ -n "$SSH_CONNECTION" ]; then
-	test -S "$SSH_AUTH_SOCK" && ln -sf "$SSH_AUTH_SOCK" "$HOME/.ssh/auth_sock"
+	test -S "$SSH_AUTH_SOCK" &&
+		[ "$(realpath "$SSH_AUTH_SOCK")" != "$(realpath "$HOME/.ssh/auth_sock")" ] &&
+		ln -sf "$SSH_AUTH_SOCK" "$HOME/.ssh/auth_sock"
 
 	export SSH_AUTH_SOCK="$HOME/.ssh/auth_sock"
 fi
