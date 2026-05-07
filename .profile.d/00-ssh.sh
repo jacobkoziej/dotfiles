096fcb31d8cfa1b22b054883ecd1e8b38f25cd53
Jacob Koziej <jacobkoziej@gmail.com>
2026-05-06 22:24:33 -0700

    refactor: have agent socket end in `.sock`

--- .profile.d/00-ssh.sh
+++ .profile.d/00-ssh.sh
@@ -1,7 +1,7 @@
 if [ -n "$SSH_CONNECTION" ]; then
 	test -S "$SSH_AUTH_SOCK" &&
-		[ "$(realpath "$SSH_AUTH_SOCK")" != "$(realpath "$HOME/.ssh/auth_sock")" ] &&
-		ln -sf "$SSH_AUTH_SOCK" "$HOME/.ssh/auth_sock"
+		[ "$(realpath "$SSH_AUTH_SOCK")" != "$(realpath "$HOME/.ssh/auth.sock")" ] &&
+		ln -sf "$SSH_AUTH_SOCK" "$HOME/.ssh/auth.sock"
 
-	export SSH_AUTH_SOCK="$HOME/.ssh/auth_sock"
+	export SSH_AUTH_SOCK="$HOME/.ssh/auth.sock"
 fi
