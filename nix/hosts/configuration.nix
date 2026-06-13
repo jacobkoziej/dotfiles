0e9d04604b663cfea51b6374eb8b66a007c4dbcf
Jacob Koziej <jacobkoziej@gmail.com>
2026-06-13 13:49:09 -0700

    fix: correct spelling of `remote-nixbld`

--- nix/hosts/configuration.nix
+++ nix/hosts/configuration.nix
@@ -33,7 +33,7 @@ in
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       trusted-users = [
-        "@remote-build"
+        "@remote-nixbld"
         "@wheel"
       ];
     };
